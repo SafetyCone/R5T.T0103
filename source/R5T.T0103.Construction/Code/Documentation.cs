c2ab90c8d350f534465b375468f2c15b09447fc5
davidcoats <david@getsafetycone.com>
2021-12-15 22:57:38 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0103.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0103.Construction
+{
+    /// <summary>
+    /// Construction project for IRepositoryOperator, IRepositoryGenerator, and related extension method bases..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0103/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0103
+{
+    /// <summary>
+    /// IRepositoryOperator, IRepositoryGenerator, and related extension method bases.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
